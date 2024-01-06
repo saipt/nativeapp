2b325c31463c58227648437e7918fd0ea7fdf6be
saipt <saipt@amazon.com>
2024-01-07 00:36:55 +0530

    pubsub addition

--- App.tsx
+++ App.tsx
@@ -1,25 +1,22 @@
 import React, {useEffect} from 'react';
-import { Button,View,Text,Alert } from 'react-native';
+import { Button,View,Text,Alert,StyleSheet,Pressable } from 'react-native';
 import {pubsub} from './src/utils/pubsub.ts'
 import {Amplify,CONNECTION_STATE_CHANGE, ConnectionState ,PubSub}  from 'aws-amplify';
 import { Hub } from 'aws-amplify/utils';
 import { Authenticator, useAuthenticator,withAuthenticator, Heading } from '@aws-amplify/ui-react-native';
 import amplifyconfig from './src/amplifyconfiguration.json';
-//Amplify.configure(amplifyconfig);
-import { fetchAuthSession } from 'aws-amplify/auth';
+import { fetchAuthSession,AuthUser } from 'aws-amplify/auth';
 import {AWSIoTProvider}  from '@aws-amplify/pubsub/';
-
-import { type AuthUser } from "aws-amplify/auth";
 import { type UseAuthenticator } from "@aws-amplify/ui-react-core";
 
 
 import awsExports from './src/aws-exports';
 //Amplify.configure(awsExports);
 import { signUp,SignOut } from 'aws-amplify/auth';
-//window.LOG_LEVEL = 'DEBUG';
+window.LOG_LEVEL = 'DEBUG';
 
-// retrieves only the current value of 'user' from 'useAuthenticator'
-const userSelector = (context) => [context.user];
+// // retrieves only the current value of 'user' from 'useAuthenticator'
+// const userSelector = (context) => [context.user];
 
 fetchAuthSession().then((info) => {
   const cognitoIdentityId = info.identityId;
@@ -27,29 +24,20 @@ fetchAuthSession().then((info) => {
 });
 
 
-// Amplify.addPluggable(new AWSIoTProvider({
-//      aws_pubsub_region: 'us-east-1',
-//      aws_pubsub_endpoint: 'wss://a2vh905c4kwt1m-ats.iot.us-east-1.amazonaws.com/mqtt',
-//    }));
-
 Hub.listen('pubsub', (data) => {
   const { payload } = data;
-  console.log("---",payload.event)
+//  console.log("---",payload.event)
   });
 
+// pubsub is defined in "./src/utils/pubsub.ts" path
 pubsub.subscribe({ topics: 'myTopic' }).subscribe({
   next: (data) => console.log('Message received', data),
   error: (error) => console.error(error),
   complete: () => console.log('Done')
 });
 
-type AppProps = {
-  signOut?: UseAuthenticator["signOut"]; //() => void;
-  user?: AuthUser;
-};
-
 async function publishMessage() {
-    const out = pubsub.publish({ topics: 'myTopic', message: { msg: 'Message from React Native' } });
+  const out = pubsub.publish({ topics: 'myTopic', message: { msg: 'Message from React Native App' } });
 }
 
 const App = () => (
@@ -60,7 +48,6 @@ const App = () => (
       title="Publish"
       color="#841584"
     />
-  </View>
+</View>
 );
-
 export default withAuthenticator(App);
